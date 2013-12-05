8c10b822d77b5d6169b36d730d64a1c63bfabae8
Magnus Mårtensson <magnus@magnusmartensson.com>
2013-12-05 23:31:02 +0100

    Cool Runnings!

--- Vanilla/Properties/AssemblyInfo.cs
+++ Vanilla/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Vanilla")]
+[assembly: AssemblyTitle("MvcApplication1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Vanilla")]
+[assembly: AssemblyProduct("MvcApplication1")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8ae60737-1f58-4436-bbbc-d366c9850c58")]
+[assembly: Guid("431b6555-9e2c-4d8e-a8bc-b30007e0b6f8")]
 
 // Version information for an assembly consists of the following four values:
 //
